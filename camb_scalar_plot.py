fdede4f59b47393fc675a75a38d1cc14185ae8b8
Daniil D <daniild@gmail.com>
2019-07-20 23:52:33 +0100

    create a Cl TT plot

--- camb_scalar_plot.py
+++ camb_scalar_plot.py
@@ -1,4 +1,7 @@
 import camb
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 params = camb.read_ini('camb_interface/camb_params.ini')
 
@@ -14,6 +17,22 @@ spectra = results.get_cmb_power_spectra(params=None, lmax=2200, spectra=['unlens
 
 unlensed = results.get_cmb_unlensed_scalar_array_dict(lmax=2200, CMB_unit='muK')
 
+tt = unlensed['TxT']
+ee = unlensed['ExE']
+te = unlensed['TxE']
 
+fig, ax = plt.subplots()
+tt = np.delete(tt, [0, 1])
+ax.plot(tt, linewidth=1)
+ax.set_xlim([None, 2500])
+ax.set_ylim([None, 7000])
+ax.set(xlabel='Multipole Moment (l)', ylabel='CMB_Unit (uK)', title='Cl TT CMB Power Spectrum')
+fig.savefig("Cl_TT.pdf")
+plt.show()
+
+# ax.plot(ee)
+# ax.set(xlabel='Multipole Moment (l)', ylabel='CMB_Unit (uK)', title='Cl EE CMB Power Spectrum')
+# fig.savefig("Cl_EE.png")
+# plt.show()
 
 print('done')
